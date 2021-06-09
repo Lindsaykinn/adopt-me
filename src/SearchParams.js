545d68f53735737cfe3a892d6eb3746ef931a3b0
Lindsay Kinn <lindsay.kinn@gmail.com>
2021-06-09 15:13:01 -0400

    added useState hook

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,18 +1,26 @@
+import { useState } from "react";
+
 const SearchParams = () => {
+  const [location, setLocation] = useState("Seattle, WA");
+  location.split(" ");
 
-  const location = "Seattle, WA";
-
-  return ( 
+  return (
     <div className="search-params">
       <form>
         <label htmlFor="location">
           Location
-          <input type="text" id="location" value={location.toUpperCase()} placeholder="Location" />
+          <input
+            type="text"
+            id="location"
+            onChange={(e) => setLocation(e.target.value)}
+            value={location[0].toUpperCase() + location.substring(1)}
+            placeholder="Location"
+          />
         </label>
         <button>Submit</button>
       </form>
     </div>
-   );
-}
- 
+  );
+};
+
 export default SearchParams;
