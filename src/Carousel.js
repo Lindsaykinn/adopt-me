24683d3810bf4bbbaf727a6341612de0e70f6a59
Lindsay Kinn <lindsay.kinn@gmail.com>
2021-06-11 17:34:53 -0400

    created Carousel component

---
+++ src/Carousel.js
@@ -0,0 +1,35 @@
+import { Component } from "react";
+
+class Carousel extends Component {
+  state = {
+    active: 0,
+  };
+
+  //if there are no images available, default images will be provided via the url below
+  static defaultProps = {
+    images: ["http://pets-images.dev-apis.com/pets/none.jpg"],
+  };
+
+  render() {
+    const { active } = this.state;
+    const { images } = this.props;
+
+    return (
+      <div className="carousel">
+        <img src={images[active]} alt="animal" />
+        <div className="carousel-smaller">
+          {images.map((photo, index) => (
+            <img
+              src={photo}
+              key={photo}
+              className={index === active ? "active" : ""}
+              alt="animal thumbnail"
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Carousel;
