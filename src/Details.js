b2a48f89d9292d9beda175300a0a218e9dfa3c1c
Lindsay Kinn <lindsay.kinn@gmail.com>
2021-06-15 11:30:06 -0400

    export default on details to incl errorboundary

--- src/Details.js
+++ src/Details.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router";
 import Carousel from "./Carousel";
+import ErrorBoundary from "./ErrorBoundary";
 
 class Details extends Component {
   state = { loading: true };
@@ -22,6 +23,7 @@ class Details extends Component {
   render() {
     const { animal, breed, city, state, description, name, images } =
       this.state;
+
     return (
       <div className="details">
         <Carousel images={images} />
@@ -38,4 +40,12 @@ class Details extends Component {
   }
 }
 
-export default withRouter(Details);
+const DetailsWithRouter = withRouter(Details);
+
+export default function DetailsWithErrorBoundary() {
+  return (
+    <ErrorBoundary>
+      <DetailsWithRouter />
+    </ErrorBoundary>
+  );
+}
